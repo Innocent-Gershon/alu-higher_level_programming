dea032c08b79c40dad30c8fb6574ebcc7f561d74
Innocent-Gershon <i.nangah@alustudent.com>
2024-07-19 05:58:53 -0700

    Done

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,12 +1,2 @@
-if [ "$#" -ne 1 ]; then
-  echo "Usage: $0 <database_name>"
-  exit 1
-fi
-
-# Assign the argument to a variable
-DATABASE_NAME=$1
-
-# Insert the new row into the table
-mysql -u root -p -e "
-USE $DATABASE_NAME;
-INSERT INTO first_table (id, name) VALUES (89, 'Best School');
+-- nserts a new row in the table first_table
+INSERT INTO first_table(id,name) VALUES(89, "Holberton School");
