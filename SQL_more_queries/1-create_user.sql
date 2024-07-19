9419e2aa6f1b59936cd515d6d3b16e38c1deb4ca
Innocent-Gershon <i.nangah@alustudent.com>
2024-07-19 08:27:45 -0700

    Done

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,11 @@
--- cc
+-- 1-create_user.sql
+
+-- Check if the user exists before creating it
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
+
+-- Grant all privileges on all databases and tables to the user
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+-- Apply the changes
 FLUSH PRIVILEGES;
+
