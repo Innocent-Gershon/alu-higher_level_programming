5d14005a167aa66c2968875bc3a3be9627d1c401
Innocent-Gershon <i.nangah@alustudent.com>
2024-07-19 06:14:27 -0700

    Done

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,9 +1,2 @@
--- nserts a new row in the table first_table
+--inserts a new row in the table first_table
 INSERT INTO first_table(id,name) VALUES(89, "Holberton School");
-if [ "$#" -ne 1 ]; then
-  echo "Usage: $0 <database_name>"
-  exit 1
-fi
-DATABASE_NAME=$1
-mysql -u root -p $DATABASE_NAME < insert_row.sql
-echo "New row inserted into table 'first_table' in database '$DATABASE_NAME'."
