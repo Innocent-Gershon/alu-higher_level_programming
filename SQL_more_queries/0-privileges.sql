ecb00ed3b5621870d6ffcfe68037c093b11de88b
Innocent-Gershon <i.nangah@alustudent.com>
2024-07-20 03:18:36 -0700

    Done

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,17 @@
--- 0-privileges.sql
+-- Create user_0d_1 and user_0d_2 if they don't already exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+
+-- Grant all privileges to user_0d_1 and user_0d_2
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
+
+-- Flush privileges to ensure that all changes take effect
+FLUSH PRIVILEGES;
+
+-- List all privileges for user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- List all privileges for user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
 
