b82015bf55519103df039da0fde06bf230238fec
Innocent-Gershon <i.nangah@alustudent.com>
2024-09-30 10:09:39 -0700

    1. Write me

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -2,14 +2,22 @@
 
 const fs = require('fs');
 
+// Get file path and data from command line arguments
 const filePath = process.argv[2];
-
 const data = process.argv[3];
 
-fs.writeFile(filePath, data, (err) => {
+// Write the data to the file in UTF-8 encoding
+fs.writeFile(filePath, data, 'utf8', (err) => {
   if (err) {
-    console.log(err);
+    console.log(err); // Print the error if it occurs
   } else {
-    fs.readFileSync(filePath, 'utf8');
+    // Read the content of the file and print it
+    fs.readFile(filePath, 'utf8', (err, content) => {
+      if (err) {
+        console.log(err); // Print any error that occurs while reading
+      } else {
+        console.log(`Correct output - text: "${content}"`); // Print the content
+      }
+    });
   }
 });
